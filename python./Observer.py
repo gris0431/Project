fddf49a6f269404bfbc2fabb6c89b1d80f089419
gris0431 <134439152+gris0431@users.noreply.github.com>
2024-05-16 04:42:00 +0300

    Update Observer.py
    Изменил работу скрипта, чтобы уменьшить вероятность ложного срабатывания

--- python./Observer.py
+++ python./Observer.py
@@ -1,74 +1,97 @@
 import numpy as np
 import cv2
-import dlib
 import os
 import time
 
-# Загрузка моделей и инициализация детектора
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-detector = dlib.get_frontal_face_detector()
-PREDICTOR_PATH1 = "./shape_predictor_68_face_landmarks.dat"
-PREDICTOR_PATH2 = "./dlib_face_recognition_resnet_model_v1.dat"
-predictor = dlib.shape_predictor(PREDICTOR_PATH1)
-face_rec_model = dlib.face_recognition_model_v1(PREDICTOR_PATH2)
 
-# Запуск видеозахвата
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+# Установка HOG детектора и экстрактора дескрипторов HOG
+hog_detector = cv2.HOGDescriptor()
+hog_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+# Функция для вычисления HOG-дескрипторов для вырезанного региона
+def get_hog_descriptor(image):
+    resized_image = cv2.resize(image, (64, 128))  # Размеры должны совпадать с требованиями HOG
+    hog_descriptor = hog_detector.compute(resized_image)
+    return hog_descriptor.flatten()
+
+
+# Настройки видеозахвата
 cv2.startWindowThread()
 cap = cv2.VideoCapture(0)
 
-# Создание каталога для фотографий, если он не существует
 if not os.path.exists('photo'):
     os.makedirs('photo')
 
-known_faces = []  # Известные лица
-tolerance = 0.6  # Порог схожести лиц для определения уникальности
-forget_after_seconds = 5  # Время в секундах, после которого лицо будет забыто
-forget_timer = {}  # Таймер забывания лиц
+known_persons = []
+frame_interval = 30  # Интервал между снимками в кадрах
+tolerance = 0.8  # Порог для косинусного сходства
 
-# Бесконечный цикл обработки видеопотока
+previous_frame = None
+frame_count = 0
+
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Обработка кадра
-    frame = cv2.resize(frame, (640, 480))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray, 1)
+    frame_count += 1
 
-    current_face_descriptors = []  # Дескрипторы текущих лиц
+    # Пропускаем кадры вплоть до достижения нужного интервала
+    if frame_count % frame_interval != 0:
+        continue
 
-    # Определение дескрипторов для каждого лица в кадре
-    for face in faces:
-        shape = predictor(gray, face)
-        face_descriptor = face_rec_model.compute_face_descriptor(frame, shape)
-        current_face_descriptors.append(face_descriptor)
+    frame_resized = cv2.resize(frame, (640, 480))
+    gray = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
 
-    # Удаление лиц, которых больше нет в кадре, из списка известных лиц
-    now = time.time()  # Текущее время
-    forgettable_faces = [face for face, last_seen in forget_timer.items() if now - last_seen > forget_after_seconds]
-    for face in forgettable_faces:
-        known_faces.remove(face)
-        del forget_timer[face]
+    # Проверка изменения в кадре
+    if previous_frame is not None:
+        diff = cv2.absdiff(previous_frame, gray)
+        if np.sum(
+                diff) < 1e5:  # Например, если изменение в кадре незначительное - можно изменить значение по необходимости
+            continue
 
-    # Определение новых лиц и съемка фотографии
-    for face_descriptor in current_face_descriptors:
-        is_new_face = all(
-            np.linalg.norm(np.array(face_descriptor) - np.array(known_face)) > tolerance for known_face in known_faces)
-        if is_new_face:
-            timestamp = time.strftime("%Y%m%d%H%M%S")
-            photo_path = os.path.join('photo', f'photo_{timestamp}.jpg')
-            cv2.imwrite(photo_path, frame)
-            known_faces.append(face_descriptor)  # Добавление нового лица в список известных лиц
-            forget_timer[face_descriptor] = time.time()  # Установка времени забывания для нового лица
+    previous_frame = gray  # Обновление предыдущего кадра
 
-    # Отображение кадра
-    cv2.imshow('frame', frame)
+    rects, weights = hog_detector.detectMultiScale(gray, winStride=(8, 8), padding=(16, 16), scale=1.05)
+    current_person_descriptors = []
+
+    for (x, y, w, h) in rects:
+        person_roi = frame_resized[y:y + h, x:x + w]
+        descriptor = get_hog_descriptor(person_roi)
+        current_person_descriptors.append(descriptor)
+
+    if current_person_descriptors:
+        if not known_persons:
+            for descriptor in current_person_descriptors:
+                known_persons.append(descriptor)
+                timestamp = time.strftime("%Y%m%d%H%M%S")
+                photo_path = os.path.join('photo', f'photo_{timestamp}.jpg')
+                cv2.imwrite(photo_path, frame_resized)
+        else:
+            is_any_new_person = False
+            for current_descriptor in current_person_descriptors:
+                if all(cosine_similarity(current_descriptor, known_descriptor) < tolerance for known_descriptor in
+                       known_persons):
+                    is_any_new_person = True
+            if is_any_new_person:
+                known_persons = current_person_descriptors  # Обновление известных силуэтов
+                timestamp = time.strftime("%Y%m%d%H%M%S")
+                photo_path = os.path.join('photo', f'photo_{timestamp}.jpg')
+                cv2.imwrite(photo_path, frame_resized)
+
+    # Отрисовка рамок
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame_resized, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame_resized)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Освобождение ресурсов
 cap.release()
 cv2.destroyAllWindows()
 cv2.waitKey(1)
