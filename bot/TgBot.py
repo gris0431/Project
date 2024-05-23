19af961610ddf45858627355486dcd27aa7567e3
gris0431 <134439152+gris0431@users.noreply.github.com>
2024-05-23 14:08:12 +0300

    Update TgBot.py

--- bot/TgBot.py
+++ bot/TgBot.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime, timedelta
 from cgitb import text
 from email import message
@@ -11,43 +10,45 @@ import random
 import cv2
 
 allowed = [
-    "701143942"
-    ]
+    "701143942",
+    "808250723"
+]
 
 bot = telebot.TeleBot('7058821742:AAFF-CXycpTOK8JNMNTK8sIRlJPjAWxuI2A')
-animals_dir = "AnimalTest"
-cam_dir = "CamTest\\"
+animals_dir = "photo/photo_network"
+cam_dir = "photo"
 images = os.listdir(animals_dir)
 for i in range(len(images)):
     print(images[i])
 
-photos_num = len(images)-1
+photos_num = len(images) - 1
 streaming = True
 learning = False
 improving = False
-names = [ "котик",
+names = ["котик",
          "песик",
          "хомячок"
-    ]
+         ]
 
-names_num = len(names)-1
+names_num = len(names) - 1
 
-@bot.message_handler(commands=['start','stop'])
+
+@bot.message_handler(commands=['start', 'stop'])
 def start(message):
     if (is_allowed(message)):
-        bot.send_message(message.chat.id, text="Доступ разрешен") 
+        bot.send_message(message.chat.id, text="Доступ разрешен")
         global streaming
         global learning
         global improving
         streaming = True
         learning = False
         improving = False
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) #наша клавиатура
-        key_start_learning = types.KeyboardButton('Начать обучение') #кнопка «Начать обучение»
-        key_start_correction = types.KeyboardButton('Улучшить точность') #кнопка «Начать обучение»
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)  # наша клавиатура
+        key_start_learning = types.KeyboardButton('Начать обучение')  # кнопка «Начать обучение»
+        key_start_correction = types.KeyboardButton('Улучшить точность')  # кнопка «Начать обучение»
         key_show_report = types.KeyboardButton('Показать отчет за последние сутки')
-        keyboard.add(key_start_learning ,key_start_correction, key_show_report)
-        bot.send_message(message.chat.id, text="Режим дежурства",reply_markup=keyboard)
+        keyboard.add(key_start_learning, key_start_correction, key_show_report)
+        bot.send_message(message.chat.id, text="Режим дежурства", reply_markup=keyboard)
         check(message)
         os.chdir("photo")
         while streaming:
@@ -55,35 +56,37 @@ def start(message):
                 current_time = time.time()
                 creation_time = os.path.getmtime(filename)
                 if (filename.endswith(".jpg")) and (current_time - creation_time < 15):
-                   bot.send_photo(message.chat.id, filename)
-                   bot.send_message(message.chat.id, text="Обнаружен человек") 
+                    bot.send_photo(message.chat.id, filename)
+                    bot.send_message(message.chat.id, text="Обнаружен человек")
     else:
-        bot.send_message(message.chat.id, text="Доступ запрещен") 
+        bot.send_message(message.chat.id, text="Доступ запрещен")
+
 
 @bot.message_handler(commands=['check'])
 def check(message):
     if (is_allowed(message)):
-        bot.send_message(message.chat.id, text="Доступ разрешен") 
+        bot.send_message(message.chat.id, text="Доступ разрешен")
         # Включаем первую камеру
         cap = cv2.VideoCapture(0)
-    # "Прогреваем" камеру, чтобы снимок не был тёмным
+        # "Прогреваем" камеру, чтобы снимок не был тёмным
         for i in range(30):
             cap.read()
-    # Делаем снимок    
+        # Делаем снимок
         ret, frame = cap.read()
-    # Записываем в файл
-        cv2.imwrite(cam_dir+'cam.png', frame)   
-    # Отключаем камеру
+        # Записываем в файл
+        cv2.imwrite(cam_dir + 'cam.png', frame)
+        # Отключаем камеру
         cap.release()
-        bot.send_photo(message.chat.id, open(cam_dir+"cam.png", 'rb'))
-    
+        bot.send_photo(message.chat.id, open(cam_dir + "cam.png", 'rb'))
+
     else:
-        bot.send_message(message.chat.id, text="Доступ запрещен")     
+        bot.send_message(message.chat.id, text="Доступ запрещен")
+
 
 @bot.message_handler(commands=['learn'])
 def learn(message):
     if (is_allowed(message)):
-        bot.send_message(message.chat.id, text="Доступ разрешен") 
+        bot.send_message(message.chat.id, text="Доступ разрешен")
         global streaming
         global learning
         streaming = False
@@ -91,19 +94,20 @@ def learn(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         back_btn = types.KeyboardButton("Закончить обучение")
         markup.add(back_btn)
-        bot.send_message(message.chat.id, text="Режим обучения",  reply_markup=markup)
+        bot.send_message(message.chat.id, text="Режим обучения", reply_markup=markup)
         i = random.randint(0, photos_num)
         j = random.randint(0, names_num)
-        bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+        bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
         bot.send_message(message.chat.id, text="Кто это?".format(names[j]))
-        
+
     else:
-        bot.send_message(message.chat.id, text="Доступ запрещен") 
-    
+        bot.send_message(message.chat.id, text="Доступ запрещен")
+
+
 @bot.message_handler(commands=['improvement'])
 def improve(message):
     if (is_allowed(message)):
-        bot.send_message(message.chat.id, text="Доступ разрешен") 
+        bot.send_message(message.chat.id, text="Доступ разрешен")
         global streaming
         global improving
         streaming = False
@@ -117,18 +121,21 @@ def improve(message):
         bot.send_message(message.chat.id, text="Режим улучшения точности", reply_markup=markup)
         i = random.randint(0, photos_num)
         j = random.randint(0, names_num)
-        bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+        bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
         bot.send_message(message.chat.id, text="Это {}?".format(names[j]))
-        
+
     else:
-        bot.send_message(message.chat.id, text="Доступ запрещен") 
-    
+        bot.send_message(message.chat.id, text="Доступ запрещен")
+
+
 @bot.message_handler(commands=['report'])
 def report(message):
     if (streaming == True):
         date = datetime.now().date() - timedelta(days=1)
-        bot.send_message(message.chat.id, text=str(date)+report_message)
-    else: wrong_command(message)    
+        bot.send_message(message.chat.id, text=str(date) + report_message)
+    else:
+        wrong_command(message)
+
 
 @bot.message_handler(commands=['yes'])
 def yes(message):
@@ -136,87 +143,102 @@ def yes(message):
         bot.send_message(message.chat.id, text="Супер")
         i = random.randint(0, photos_num)
         j = random.randint(0, names_num)
-        bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+        bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
         bot.send_message(message.chat.id, text="Это {}?".format(names[j]))
-    else: wrong_command(message)
-    
+    else:
+        wrong_command(message)
+
+
 @bot.message_handler(commands=['no'])
 def no(message):
     if (improving == True):
         bot.send_message(message.chat.id, text="Принято")
         i = random.randint(0, photos_num)
         j = random.randint(0, names_num)
-        bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+        bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
         bot.send_message(message.chat.id, text="Это {}?".format(names[j]))
-    else: wrong_command(message)
-    
+    else:
+        wrong_command(message)
+
+
 @bot.message_handler(commands=['skip'])
 def skip(message):
     if (improving == True):
         bot.send_message(message.chat.id, text="Прошу прощения")
         i = random.randint(0, photos_num)
         j = random.randint(0, names_num)
-        bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+        bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
         bot.send_message(message.chat.id, text="Это {}?".format(names[j]))
-    else: wrong_command(message) 
+    else:
+        wrong_command(message)
+
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    if(streaming == True):
-        bot.send_message(message.chat.id, text="/learn - начать обучение\n/improvement - начать улучшение точности\n/report - получить отчет за последние сутки\n/check - получить кадр с камеры")
-    
-    elif(learning == True):
+    if (streaming == True):
+        bot.send_message(message.chat.id,
+                         text="/learn - начать обучение\n/improvement - начать улучшение точности\n/report - получить отчет за последние сутки\n/check - получить кадр с камеры")
+
+    elif (learning == True):
         bot.send_message(message.chat.id, text="/stop - закончить обучение\n/check - получить кадр с камеры")
-        
+
     else:
-        bot.send_message(message.chat.id, text="/yes - подтвердить идентификацию\n/no - опровергнуть идентификацию\n/skip - пропустить некорректное изображение\n/stop - закончить улучшение точности\n/check - получить кадр с камеры")
+        bot.send_message(message.chat.id,
+                         text="/yes - подтвердить идентификацию\n/no - опровергнуть идентификацию\n/skip - пропустить некорректное изображение\n/stop - закончить улучшение точности\n/check - получить кадр с камеры")
+
 
 @bot.message_handler(content_types=['text'])
 def func(message):
-    
-    if(message.text == "Улучшить точность"):
+    if (message.text == "Улучшить точность"):
         improve(message)
-     
-    elif(message.text == "Начать обучение"):
+
+    elif (message.text == "Начать обучение"):
         learn(message)
-        
-    elif(message.text in ["Закончить обучение", "Закончить улучшение точности"]):
+
+    elif (message.text in ["Закончить обучение", "Закончить улучшение точности"]):
         start(message)
-            
-    elif(message.text == "Да"):
+
+    elif (message.text == "Да"):
         yes(message)
-        
-    elif(message.text == "Нет"):
+
+    elif (message.text == "Нет"):
         no(message)
-        
-    elif(message.text == "Нет человека"):       
+
+    elif (message.text == "Нет человека"):
         skip(message)
-        
-    elif(message.text == 'Показать отчет за последние сутки'):
+
+    elif (message.text == 'Показать отчет за последние сутки'):
         report(message)
-        
-    elif(learning == True and message.text not in names):
+
+    elif (learning == True and message.text not in names):
         bot.send_message(message.chat.id, text="Неверный формат, введите существующее имя")
-    
-    elif(learning == True and message.text in names):
+
+    elif (learning == True and message.text in names):
         bot.send_message(message.chat.id, text="Я запомню")
         i = random.randint(0, photos_num)
-        bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+        bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
         bot.send_message(message.chat.id, text="Кто это?")
-        
-    else: wrong_command(message) 
+
+    else:
+        wrong_command(message)
+
 
 def stream(message):
     i = random.randint(0, photos_num)
-    bot.send_photo(message.chat.id, open(animals_dir+"\\"+images[i], 'rb'))
+    bot.send_photo(message.chat.id, open(animals_dir + "\\" + images[i], 'rb'))
     bot.send_message(message.chat.id, text="Обнаружено движение")
-    
+
+
 def wrong_command(message):
-    bot.send_message(message.chat.id, text="Неверная команда, для того, чтобы посмотреть список доступных команд напишите /help")
-    
+    bot.send_message(message.chat.id,
+                     text="Неверная команда, для того, чтобы посмотреть список доступных команд напишите /help")
+
+
 def is_allowed(message):
-    if (message.chat.id in allowed):
+    print(message.chat.id)
+    if (str(message.chat.id) in allowed):
         return True
     return False
-    
+
+
 bot.polling(none_stop=True, interval=0)
