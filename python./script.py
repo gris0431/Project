76e68ef316c4f9a58062db7dcf4c87e9b6507cdb
gris0431 <134439152+gris0431@users.noreply.github.com>
2024-05-30 02:04:41 +0300

    Update script.py

--- python./script.py
+++ python./script.py
@@ -1,31 +1,48 @@
 import concurrent.futures
 import subprocess
 import time
+import threading
 
 def run_comb_script():
-    # Запуск скрипта comb.py
     try:
         subprocess.run(["python", "comb.py"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running comb.py: {e}")
 
-def run_neural_network_script():
-    # Запуск скрипта neuralnetwork.py с периодичностью
+def run_neural_network_script(stop_event):
     try:
-        while True:
+        while not stop_event.is_set():
             subprocess.run(["python", "neuralnetwork.py"], check=True)
+            time.sleep(1)
     except subprocess.CalledProcessError as e:
         print(f"Error running neuralnetwork.py: {e}")
 
-# Запуск обоих скриптов параллельно
+def run_main_script(stop_event):
+    try:
+        while not stop_event.is_set():
+            subprocess.run(["python", "neu2.py"], check=True)
+            time.sleep(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running neu2.py: {e}")
+
+def run_bot_script(stop_event):
+    try:
+        while not stop_event.is_set():
+            subprocess.run(["python", "bot.py"], check=True)
+            time.sleep(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running bot.py: {e}")
+
+stop_event = threading.Event()
+
 with concurrent.futures.ThreadPoolExecutor() as executor:
     future_comb = executor.submit(run_comb_script)
-    future_neural_network = executor.submit(run_neural_network_script)
+    future_neural_network = executor.submit(run_neural_network_script, stop_event)
+    future_main = executor.submit(run_main_script, stop_event)
+    future_bot = executor.submit(run_bot_script, stop_event)
 
-    # Ожидание завершения обоих процессов
     concurrent.futures.wait([future_comb])
 
-    # Если future_comb завершен, отменить future_neural_network
-    future_neural_network.cancel()
+    stop_event.set()
 
-print("Both scripts have finished executing")
+print("comb.py has finished executing, other scripts have been stopped")
