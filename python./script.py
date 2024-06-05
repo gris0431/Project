245c7de17c9c50b0679d226ce3708bb6215fbcdd
gris0431 <134439152+gris0431@users.noreply.github.com>
2024-06-05 05:08:04 +0300

    Update script.py

--- python./script.py
+++ python./script.py
@@ -2,44 +2,47 @@ import concurrent.futures
 import subprocess
 import time
 import threading
+import sys
+import os
 
-def run_comb_script():
+def run_comb_script(python_executable):
     try:
-        subprocess.run(["python", "Observer.py"], check=True)
+        subprocess.run([python_executable, "Observer.py"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running comb.py: {e}")
 
-def run_neural_network_script(stop_event):
+def run_neural_network_script(stop_event, python_executable):
     try:
         while not stop_event.is_set():
-            subprocess.run(["python", "neuralnetwork.py"], check=True)
+            subprocess.run([python_executable, "neuralnetwork.py"], check=True)
             time.sleep(1)
     except subprocess.CalledProcessError as e:
         print(f"Error running neuralnetwork.py: {e}")
 
-def run_main_script(stop_event):
+def run_main_script(stop_event, python_executable):
     try:
         while not stop_event.is_set():
-            subprocess.run(["python", "neu2.py"], check=True)
+            subprocess.run([python_executable, "neu2.py"], check=True)
             time.sleep(1)
     except subprocess.CalledProcessError as e:
         print(f"Error running neu2.py: {e}")
 
-def run_bot_script(stop_event):
+def run_bot_script(stop_event, python_executable):
     try:
         while not stop_event.is_set():
-            subprocess.run(["python", "bot.py"], check=True)
+            subprocess.run([python_executable, "bot.py"], check=True)
             time.sleep(1)
     except subprocess.CalledProcessError as e:
         print(f"Error running bot.py: {e}")
 
 stop_event = threading.Event()
+python_executable = sys.executable
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    future_comb = executor.submit(run_comb_script)
-    future_neural_network = executor.submit(run_neural_network_script, stop_event)
-    future_main = executor.submit(run_main_script, stop_event)
-    future_bot = executor.submit(run_bot_script, stop_event)
+    future_comb = executor.submit(run_comb_script, python_executable)
+    future_neural_network = executor.submit(run_neural_network_script, stop_event, python_executable)
+    future_main = executor.submit(run_main_script, stop_event, python_executable)
+    future_bot = executor.submit(run_bot_script, stop_event, python_executable)
 
     concurrent.futures.wait([future_comb])
 
